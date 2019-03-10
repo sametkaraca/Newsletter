5d289f21eb4b91d62d5965ff801d6fa7fba17e0a
Sammy <samet.karaca.no@gmail.com>
2019-03-10 18:31:44 +0100

    Procfile has been added

--- app.js
+++ app.js
@@ -3,8 +3,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const request = require("request")
-const port = 3000
-
 const app = express()
 
 //TODO: We need a static file stuff here to send the browser.
@@ -12,24 +10,16 @@ const app = express()
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true})) //We added body parser here
 
-
-
-
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/signup.html'); 
-})  
+})   
 
-//We set a POST route here. We reach the DOM element by calling his name with body parser
-app.post("/", function(req,res){
-
-  var firstName = req.body.fName
-  var lastName = req.body.lName
-  var email = req.body.email
-
-})
-
-app.listen(port, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server is running on port: " + port)
 })
 
 /* We use body-perser to grab the form data that was sent by post*/
+
+//API KEY : e479b3ea6595891ed4b7b836ea78e18d-us20
+//List ID: 9116cb2b93
+
